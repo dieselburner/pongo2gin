77eb94c2a61fd63a39b399ff0e55b1643c856f83
Selman TUNÇ <selmantunc@yandex.com>
2022-03-08 10:24:23 +0300

    Update middleware.go

--- v4/middleware.go
+++ v4/middleware.go
@@ -1,3 +1,6 @@
+// Package pongo2gin is a template renderer that can be used with the Gin
+// web framework https://github.com/gin-gonic/gin it uses the Pongo2 template
+// library https://github.com/flosch/pongo2
 package pongo2gin
 
 import (
@@ -9,53 +12,73 @@ import (
 	"github.com/gin-gonic/gin/render"
 )
 
-//PongoRender struct init
-type PongoRender struct {
-	TmplDir string
+// RenderOptions is used to configure the renderer.
+type RenderOptions struct {
+	TemplateDir string
+	TemplateSet *pongo2.TemplateSet
+	ContentType string
 }
 
-//TemplatePath html files path
-func TemplatePath(tmplDir string) *PongoRender {
-	return &PongoRender{
-		TmplDir: tmplDir,
-	}
-}
-
-//Instance init
-func (p *PongoRender) Instance(name string, data interface{}) render.Render {
-	var template *pongo2.Template
-	fileName := path.Join(p.TmplDir, name)
-
-	if gin.Mode() == gin.DebugMode {
-		template = pongo2.Must(pongo2.FromFile(fileName))
-	} else {
-		template = pongo2.Must(pongo2.FromCache(fileName))
-	}
-
-	return &PongoHTML{
-		Template: template,
-		Name:     name,
-		Data:     data.(pongo2.Context),
-	}
-}
-
-//PongoHTML strcut
-type PongoHTML struct {
+// Pongo2Render is a custom Gin template renderer using Pongo2.
+type Pongo2Render struct {
+	Options  *RenderOptions
 	Template *pongo2.Template
-	Name     string
-	Data     pongo2.Context
+	Context  pongo2.Context
 }
 
-//Render for gin interface  render override
-func (p *PongoHTML) Render(w http.ResponseWriter) error {
+// New creates a new Pongo2Render instance with custom Options.
+func New(options RenderOptions) *Pongo2Render {
+	return &Pongo2Render{
+		Options: &options,
+	}
+}
+
+// Default creates a Pongo2Render instance with default options.
+func Default() *Pongo2Render {
+	return New(RenderOptions{
+		TemplateDir: "templates",
+		TemplateSet: nil,
+		ContentType: "text/html; charset=utf-8",
+	})
+}
+
+// Instance should return a new Pongo2Render struct per request and prepare
+// the template by either loading it from disk or using pongo2's cache.
+func (p Pongo2Render) Instance(name string, data interface{}) render.Render {
+	var template *pongo2.Template
+
+	filename := path.Join(p.Options.TemplateDir, name)
+
+	if p.Options.TemplateSet != nil {
+		template = pongo2.Must(p.Options.TemplateSet.FromFile(filename))
+		data.(pongo2.Context).Update(p.Options.TemplateSet.Globals)
+	} else {
+		if gin.Mode() == "debug" {
+			template = pongo2.Must(pongo2.FromFile(filename))
+		} else {
+			template = pongo2.Must(pongo2.FromCache(filename))
+		}
+	}
+
+	return Pongo2Render{
+		Template: template,
+		Context:  data.(pongo2.Context),
+		Options:  p.Options,
+	}
+}
+
+// Render should render the template to the response.
+func (p Pongo2Render) Render(w http.ResponseWriter) error {
 	p.WriteContentType(w)
-	return p.Template.ExecuteWriter(p.Data, w)
+	err := p.Template.ExecuteWriter(p.Context, w)
+	return err
 }
 
-//WriteContentType  for gin interface  WriteContentType override
-func (p *PongoHTML) WriteContentType(w http.ResponseWriter) {
+// WriteContentType should add the Content-Type header to the response
+// when not set yet.
+func (p Pongo2Render) WriteContentType(w http.ResponseWriter) {
 	header := w.Header()
 	if val := header["Content-Type"]; len(val) == 0 {
-		header["Content-Type"] = []string{"text/html; charset=utf-8"}
+		header["Content-Type"] = []string{p.Options.ContentType}
 	}
 }
